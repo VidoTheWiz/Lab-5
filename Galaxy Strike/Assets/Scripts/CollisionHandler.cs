08153b589017609922c274440b2df193323c3990
VidoTheWiz <mcgreg500@morrisville.edu>
2025-10-29 19:28:21 -0400

    Collision and trigger detection added

---
+++ Galaxy Strike/Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class CollisionHandler : MonoBehaviour
+{
+   
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Hit " + other.name);
+        Debug.Log($"Hit { other.gameObject.name}");
+    }
+
+}
