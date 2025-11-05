93061736ff7c23c00f09d60490bf324e68283966
VidoTheWiz <mcgreg500@morrisville.edu>
2025-11-04 23:04:01 -0500

    Explosion prefab variants created and tested.

--- Galaxy Strike/Assets/Scripts/CollisionHandler.cs
+++ Galaxy Strike/Assets/Scripts/CollisionHandler.cs
@@ -2,11 +2,12 @@ using UnityEngine;
 
 public class CollisionHandler : MonoBehaviour
 {
-   
+    [SerializeField] GameObject destroyedVFX;
+
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Hit " + other.name);
-        Debug.Log($"Hit { other.gameObject.name}");
+        Instantiate(destroyedVFX, transform.position, Quaternion.identity);
+        Destroy(gameObject);
     }
 
 }
