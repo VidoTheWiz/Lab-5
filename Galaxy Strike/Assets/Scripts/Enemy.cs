83bce5ed77d11298516efb771f638ae27bbab641
VidoTheWiz <mcgreg500@morrisville.edu>
2025-11-05 17:25:25 -0500

    Score updated using public method between classes/Scoreboard UI added and linked to score manager

--- Galaxy Strike/Assets/Scripts/Enemy.cs
+++ Galaxy Strike/Assets/Scripts/Enemy.cs
@@ -1,22 +1,32 @@
+using System;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject destroyedVFX;
     [SerializeField] int hitPoints = 3;
+    [SerializeField] int scoreValue = 10;
 
-    private void OnParticleCollision(GameObject other)
+    Scoreboard scoreboard;
+
+    private void Start()
+    {
+        scoreboard = FindFirstObjectByType<Scoreboard>();
+    }
+
+    void OnParticleCollision(GameObject other)
     {
         ProcessHit();
 
     }
 
-    private void ProcessHit()
+     void ProcessHit()
     {
         hitPoints--;
 
         if (hitPoints <= 0)
         {
+            scoreboard.IncreaseScore(scoreValue);
             Instantiate(destroyedVFX, transform.position, Quaternion.identity);
             Destroy(this.gameObject);
         }
